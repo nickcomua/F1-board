c338719171464ea1692c7cb453ae68cf5813a30a
nickcomua <61518319+nickcomua@users.noreply.github.com>
2023-04-05 12:58:19 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,153 @@
+<script lang="ts">
+	// import '@picocss/pico'
+</script>
+
+<main class="container" >
+    <h1>WARC [Your ad can be here] [icon of some shit 1]</h1>
+    <div class="grid">
+        <div>
+			<h2>Current circle</h2>
+            <table>
+                <thead>
+                  <tr>
+                    <th scope="col">Team</th>
+                    <th scope="col">Time</th>
+                    <th scope="col">Delta</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">Red racer</th>
+                    <td>4.1</td>
+                    <td>+0.3</td>
+                  </tr>
+                </tbody>
+                <!-- <tfoot>
+                  <tr>
+                    <th scope="col">#</th>
+                    <td scope="col">Total</td>
+                    <td scope="col">Total</td>
+                    <td scope="col">Total</td>
+                    <td scope="col">Total</td>
+                    <td scope="col">Total</td>
+                  </tr>
+                </tfoot> -->
+              </table>
+			<h2>Teams rating</h2>
+            <table>
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Team</th>
+                    <th scope="col">Time</th>
+                    <th scope="col">Delta</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Red racer</td>
+                    <td>4.1</td>
+                    <td>+0.3</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>IDK</td>
+                    <td>5.2</td>
+                    <td>+0.2</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>DM</td>
+                    <td>6.7</td>
+                    <td>+0.4</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">4</th>
+                    <td>Mhuahaha</td>
+                    <td>11</td>
+                    <td>+0.3</td>
+                  </tr>
+                </tbody>
+                <!-- <tfoot>
+                  <tr>
+                    <th scope="col">#</th>
+                    <td scope="col">Total</td>
+                    <td scope="col">Total</td>
+                    <td scope="col">Total</td>
+                    <td scope="col">Total</td>
+                    <td scope="col">Total</td>
+                  </tr>
+                </tfoot> -->
+              </table>
+		</div>
+		<div>
+			<h2>Last circle</h2>
+            <table>
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Team</th>
+                    <th scope="col">Time</th>
+                    <th scope="col">Delta</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Red racer</td>
+                    <td>4.1</td>
+                    <td>+0.3</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>IDK</td>
+                    <td>5.2</td>
+                    <td>+0.2</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>DM</td>
+                    <td>6.7</td>
+                    <td>+0.4</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">4</th>
+                    <td>Mhuahaha</td>
+                    <td>11</td>
+                    <td>+0.3</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">5</th>
+                    <td>Mhuahaha</td>
+                    <td>11</td>
+                    <td>+0.3</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">6</th>
+                    <td>Mhuahaha</td>
+                    <td>11</td>
+                    <td>+0.3</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">7</th>
+                    <td>Mhuahaha</td>
+                    <td>11</td>
+                    <td>+0.3</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">8</th>
+                    <td>Mhuahaha</td>
+                    <td>11</td>
+                    <td>+0.3</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">9</th>
+                    <td>Mhuahaha</td>
+                    <td>11</td>
+                    <td>+0.3</td>
+                  </tr>
+                </tbody>
+		</div>
+	</div>
+</main>
